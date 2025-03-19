bd46ffaa698c72705787fa8bf8d1ce180c9cac2e
ridwanyinus <ridwanyinusa15@gmail.com>
2025-03-19 06:43:35 +0100

    fix: fix loader loading indefinetly on mobile devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -77,28 +77,28 @@ const Hero = () => {
       borderRadius: '0% 0% 40% 10%',
     });
 
-    gsap.from('#video-frame', {
-      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
-      borderRadius: '0% 0% 0% 0%',
+    if (window.innerWidth > 1024) {
+      gsap.from('#video-frame', {
+        clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+        borderRadius: '0% 0% 0% 0%',
 
-      ease: 'power1.inOut',
+        ease: 'power1.inOut',
 
-      scrollTrigger: {
-        trigger: '#video-frame',
-        start: 'center center',
-        end: 'bottom center',
-        scrub: true,
-      },
-    });
+        scrollTrigger: {
+          trigger: '#video-frame',
+          start: 'center center',
+          end: 'bottom center',
+          scrub: true,
+        },
+      });
+    }
   });
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
-      <div className={`loader ${isLoading ? '' : 'hidden'}`}>
-        <Loader />
-      </div>
+      {isLoading && <Loader />}
 
       <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
         <div>
