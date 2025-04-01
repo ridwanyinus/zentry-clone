361563672b73b66dba57e79d1dbaea3144c7f85d
ridwanyinus <ridwanyinusa15@gmail.com>
2025-04-01 19:04:29 +0100

    refactor: remove space in class

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden '>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <Navbar />
       <Hero />
       <About />
